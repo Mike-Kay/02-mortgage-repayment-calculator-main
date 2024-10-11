5752c799d8b1c339ae8af443d1ced10a2f1012df
Mike Kay <michaelkay010@gmail.com>
2024-10-11 13:23:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Frontend Mentor - Mortgage repayment calculator solution
+
+This is a solution to the [Mortgage repayment calculator challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/mortgage-repayment-calculator-Galx1LXK73). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Input mortgage information and see monthly repayment and total repayment amounts after submitting the form
+- See form validation messages if any field is incomplete
+- Complete the form only using their keyboard
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./design/Screenshot/Screenshot-1.jpg)
+![](./design/Screenshot/Screenshot-2.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Pure Javascript
+
+### What I learned
+
+Due to the limitation of styles that can be applied to default radio input, I learned how to create keyboard accessible custom radio buttons.
+
+### Useful resources
+
+- [Custom radio button](https://www.a11ywithlindsey.com/blog/create-custom-keyboard-accessible-radio-buttons) - This is an amazing article on how to create keyboard accessible radio buttons. I'd recommend it to anyone still learning this concept.
+- [JS Mortgage Calculator formula](https://codepen.io/andarch/pen/qBxpeY) - This helped me implement the formula required for mortgage calculation.
+
+## Author
+
+- Frontend Mentor - [@Mike-Kay](https://www.frontendmentor.io/profile/Mike-Kay)
+- Twitter - [@Mike_Kay1](https://www.twitter.com/@Mike_Kay1)
---
+++ app.js
@@ -0,0 +1,93 @@
+const form = document.querySelector(".mort-form");
+const formNumInputs = [...document.querySelectorAll(`input[type="text"]`)];
+const formRadInputs = [...document.querySelectorAll(`input[type="radio"]`)];
+const formResults = document.querySelector(".form-results");
+const monthlyRepayment = document.querySelector(".monthly-repayment");
+const termRepayment = document.querySelector(".term-repayment");
+const radInputContainer = document.querySelector(".radio-container");
+const clearAllBtn = document.querySelector(".clearAll-btn");
+
+formNumInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    input.parentElement.classList.remove("show-input-error");
+  });
+});
+
+let mortgageType;
+formRadInputs.forEach((radInput) => {
+  radInput.addEventListener("change", () => {
+    if (radInput.checked) {
+      mortgageType = radInput.value;
+      radInputContainer.classList.remove("show-input-error");
+    }
+  });
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  let amount;
+  let term;
+  let interestRate;
+
+  // Validation
+  formNumInputs.forEach((input) => {
+    const value = input.value;
+    let checkValue = value / 1;
+    checkValue = Number.isNaN(checkValue, NaN);
+    console.log(checkValue);
+    if (!value) {
+      input.parentElement.classList.add("show-input-error");
+      input.nextElementSibling.nextElementSibling.textContent =
+        "This field is required";
+    } else {
+      if (checkValue) {
+        input.parentElement.classList.add("show-input-error");
+        input.nextElementSibling.nextElementSibling.textContent =
+          "Please enter a valid number";
+      } else if (value <= 0) {
+        input.parentElement.classList.add("show-input-error");
+        input.nextElementSibling.nextElementSibling.textContent =
+          "Value must be greater than zero(0)";
+      } else {
+        if (input.classList.contains("amt-input")) amount = value;
+        else if (input.classList.contains("term-input")) term = value;
+        else interestRate = value;
+      }
+    }
+  });
+  if (!mortgageType) radInputContainer.classList.add("show-input-error");
+
+  // Mortgage Calc
+  if (amount && term && interestRate && mortgageType)
+    calculateMortgage(amount, term, interestRate, mortgageType);
+});
+
+// Mortgage Calc Function
+const calculateMortgage = (amount, term, interest, type) => {
+  interest = interest / 100 / 12;
+  const paymentMonths = term * 12;
+  const mortgageCalc =
+    (amount * interest * Math.pow(1 + interest, paymentMonths)) /
+    (Math.pow(1 + interest, paymentMonths) - 1);
+  const repaymentTotal = mortgageCalc * paymentMonths;
+
+  const interestTotal = repaymentTotal - amount;
+  const interestPerMonth = interestTotal / paymentMonths;
+  let termPayment;
+  let monthlyPayment;
+  if (type == "repayment") {
+    termPayment = repaymentTotal;
+    monthlyPayment = mortgageCalc;
+  } else {
+    termPayment = interestTotal;
+    monthlyPayment = interestPerMonth;
+  }
+  formResults.classList.add("show");
+  monthlyRepayment.textContent = `£${monthlyPayment.toFixed(2)}`;
+  termRepayment.textContent = `£${termPayment.toFixed(2)}`;
+};
+
+clearAllBtn.addEventListener("click", () => {
+  formResults.classList.remove("show");
+  mortgageType = "";
+});
